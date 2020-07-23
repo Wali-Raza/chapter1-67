8a4cb35e6e2cb8aaf952d1f1687025b7aec5ab59
wali raza <walimanzoor321@gmail.com>
2020-07-23 22:28:19 +0500

    ready to commit

---
+++ ch-58 -to- 67/app.js
@@ -0,0 +1,64 @@
+ var main=document.getElementById("main-content")
+
+console.log(main)
+
+var rend=document.getElementById("rend")
+var R=document.getElementsByClassName("render")
+rend.innerHTML=rend.nodeValue;
+console.log(R)
+     
+
+    
+    
+    var firstN = document.getElementById("firstN");
+    var lastN=document.getElementById("lastN");
+     var emailID=document.getElementById("emailID");
+         document.getElementById("myButton").addEventListener("click", function() {
+       
+
+    
+       document.getElementById("last").innerHTML=lastN.value
+       document.getElementById("first").innerHTML=firstN.value
+
+      
+        document.getElementById("email").innerHTML=emailID.value
+   
+
+});
+  
+
+   var form  = document.getElementById("form-content")
+    var f =form.childNodes;    
+   
+    console.log(f)
+   
+ var lastNode=document.getElementById("lastN")
+ var ls=(lastNode.nodeType)
+ console.log(ls)
+
+
+var lastN=document.getElementById("main-content")
+console.log(lastN.childNodes[1])
+var lastN=document.getElementById("main-content")
+console.log(lastN.childNodes[0])
+var lastN=document.getElementById("main-content")
+console.log(lastN.childNodes[2])
+
+
+var  Ar= document.getElementById("email")
+console.log(Ar.nodeType)
+
+var  Ar= document.getElementById("email")
+console.log(Ar.childNodes[0].nodeType)
+
+//  var companyNode = document.getElementById("main-content").item(0);
+//  var  ChildNodes =companyNode.getChildNodes();
+//  var i=0;
+
+//  for (var int = 0; int < companyChildNodes.getLength(); i++) {
+//      var node = companyChildNodes.item(int);
+//      if (node.getNodeType() == Node.ELEMENT_NODE && Objects.equals("staff", node.getNodeName())) {
+//  0        System.out.println(node.getTextContent());
+//          break;
+//      }
+//  
